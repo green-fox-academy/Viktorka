33576d7e76a157e57464a45184664f6b5fc94bc3
heshrian <kamburovviktor@gmail.com>
2019-06-27 12:02:03 +0200

    introduction

---
+++ week-01/day-4/workshop/introduction.ts
@@ -0,0 +1,6 @@
+let a:any = "Viktor Kamburov";
+let b:number = 28;
+let c:number = 1.84;
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ week-01/day-4/workshop/play.ts
@@ -0,0 +1,6 @@
+console.log (1 + 1)
+console.log (.5 * 3)
+console.log (.5 * 4)
+console.log (6 / 2)
+console.log ( 3 **3 )
+console.log (.5)
