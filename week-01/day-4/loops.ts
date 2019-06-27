08b4464651918c9668f9c022a2aaaea80236102d
heshrian <kamburovviktor@gmail.com>
2019-06-27 11:14:03 +0200

    loops2

--- week-01/day-4/loops.ts
+++ week-01/day-4/loops.ts
@@ -1,7 +1,13 @@
 let times:number=0
 
 
-while(times < 10){
+while(times < 3){
     console.log("nota", times);
     times ++;
+}
+
+
+
+for (let iterator:number =0; iterator < 100; iterator ++ ){
+    console.log(iterator);
 }
