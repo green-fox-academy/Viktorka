8e0227b9f5e2a74c93b9d73bdb5b75cac84e60e5
heshrian <kamburovviktor@gmail.com>
2019-06-27 16:41:53 +0200

    cheat

---
+++ week-01/day-4/workshop/wontcheat.ts
@@ -0,0 +1,10 @@
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let a:number = 0
+let b:string = "I won't cheat on the exam!"
+
+while (a<100){
+    console.log(b);
+    a=a+1 
+}
