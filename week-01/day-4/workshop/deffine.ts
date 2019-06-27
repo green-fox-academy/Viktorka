0a51f5df154bc09fcbb92fb0f4473b3f67fb755d
heshrian <kamburovviktor@gmail.com>
2019-06-27 14:16:14 +0200

    not married

---
+++ week-01/day-4/workshop/deffine.ts
@@ -0,0 +1,11 @@
+let myName:string ="Viktor";
+let myAge:number = 28;
+let myHeight:number =1.84;
+let meMarried:boolean = true;
+
+
+console.log('My name is '+ myName);
+console.log('My age is '+ myAge);
+console.log('My height is '+ myHeight, 'm');
+console.log("Not married, deffo not with children");
+
