3188dfc89c09a41dbfaa994c3d7a0959608990df
heshrian <kamburovviktor@gmail.com>
2019-06-27 12:27:25 +0200

    %

---
+++ week-01/day-4/workshop/coding.ts
@@ -0,0 +1,10 @@
+let a:number = 6; //coding hours a day
+let b:number = 17; // weeks long semester
+let c:number = 5; // workdays
+let d:number = 52; //weekly work hours
+let e:number = d/c; //daily work hours
+let f:number = (a/e)*100; //% of coding hours in a semester
+
+
+console.log(a*b*c+(' hours are spent with coding'))
+console.log(f+(' % percentage of hours are spent with coding in a semester'))
