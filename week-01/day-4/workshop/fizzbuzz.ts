e6fea8e7b1fa13346acd6947be4ea1700756f0a4
heshrian <kamburovviktor@gmail.com>
2019-06-27 17:30:29 +0200

    parametric-ave need help

---
+++ week-01/day-4/workshop/fizzbuzz.ts
@@ -0,0 +1,5 @@
+let a: number = 0;
+for (a < 100) {
+  console.log(a); 
+  a++;
+}
---
+++ week-01/day-4/workshop/paraave.ts
@@ -0,0 +1,12 @@
+// Write a program that calculates the sum and the average from 1 to a given number.
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+
+let input: number = 5;
+let sum: number = 0;
+
+for (let i: number = 1; i < (input + 1); i++) {
+    sum = sum + i;
+}
+
+console.log('Sum: ' + sum + ', Average: ' + (sum/input));
