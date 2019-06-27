f60696d1accc6944dedc7690c6e77b59e4a1ebe2
heshrian <kamburovviktor@gmail.com>
2019-06-27 10:21:51 +0200

    commit2

---
+++ week-01/day-4/variables.ts
@@ -0,0 +1,11 @@
+const pi: number = 3.14;
+let times: number = 10000;
+let myName:string = "Viktor";
+
+console.log(times);
+console.log(myName);
+
+let itCanBeUndefined: number;
+
+console.log(itCanBeUndefined);
+console.log(myName + 1);
