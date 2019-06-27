4085353735a2e7fc10ec9b5f780bd1ca0d70ea2b
heshrian <kamburovviktor@gmail.com>
2019-06-27 15:39:02 +0200

    secsinday

--- week-01/day-4/workshop/secinday.ts
+++ week-01/day-4/workshop/secinday.ts
@@ -5,7 +5,10 @@ let currentSeconds: number = 42;
 // Write a program that prints the remaining seconds (as an integer) from a
 // day if the current time is represented by these variables
 
-let leftOverHours:number = 24 - currentHours
+let leftOverHours:number = 23 - currentHours
 let leftOverMinutes: number = 60 - currentMinutes;
 let leftOverSeconds: number = 60 - currentSeconds;
 
+leftOverHours*60*60+leftOverMinutes*60+leftOverSeconds
+
+console.log(leftOverHours*60*60+leftOverMinutes*60+leftOverSeconds);
