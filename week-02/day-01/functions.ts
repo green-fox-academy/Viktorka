a7d09826aee033c4030e50dbe775ebe37bdfd08e
heshrian <kamburovviktor@gmail.com>
2019-07-01 12:00:43 +0200

    doubleupfunction

---
+++ week-02/day-01/functions.ts
@@ -0,0 +1,18 @@
+/* let numberToMultiply: number = 12;
+
+let twice: number = numberToMultiply * 2;
+console.log(twice);
+
+let anotherNumber:number = 25;
+
+let twiceAnotherOne:number = anotherNumber * 2;
+
+console.log(twiceAnotherOne); */
+
+function printNumberAndChangeIt(inputNumber: number) {
+    let doubledValue: number = inputNumber * 2;
+    console.log(doubledValue);
+}
+
+printNumberAndChangeIt(13);
+printNumberAndChangeIt(11);
--- week-02/day-01/recap.ts
+++ week-02/day-01/recap.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-let myName: string = "Viktor";
+/*let myName: string = "Viktor";
 let yourName: string = "Viktor";
 let hisName: string = "Viktor";
 
@@ -16,3 +16,30 @@ cutePuppy.forEach
         console.log(puppy);
     })
 
+let diffArray:any[]=["Viktor", true, true];
+
+console.log("before "+diffArray);
+
+diffArray.splice( 0, 1, "marks");
+
+console.log("after "+ diffArray);  
+
+let doubles:number []=[1,2,3,4,5].map(function(i) {
+    return i ** 2;
+})
+
+console.log(doubles) 
+
+let odds:number []=[1,2,3,4,5].filter(function(item){
+    return item % 2 !== 0;
+})
+
+console.log("Odds are "+odds); 
+
+let magic:number []=[];
+console.log(magic[-100]); */
+
+let array1:number []=[1,2];
+let array2:number []=[3,4];
+let array3:number []=[...array1,...array2];
+console.log(array3);
