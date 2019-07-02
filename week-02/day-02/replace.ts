676ff74df0ca8af19f8863388288242d85a2dd43
heshrian <kamburovviktor@gmail.com>
2019-07-02 10:50:37 +0200

    matrev

---
+++ week-02/day-02/replace.ts
@@ -0,0 +1,2 @@
+var obj = { 0: 'a', 1: 'b', 2: 'c' };
+console.log(Object.values(obj)); // ['a', 'b', 'c']
---
+++ week-02/day-02/replaceex.ts
@@ -0,0 +1,12 @@
+'use strict';
+let toReplace = /dishwasher/gi;
+let example: string = 'In a dishwasher far far away';
+
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+
+let newStr = example.replace(toReplace, "galaxy" )
+console.log(newStr);
+
+export = example;
