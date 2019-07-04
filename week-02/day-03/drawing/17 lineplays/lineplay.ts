c8f01ae5a49556eeb454e6a51c09ec7a25388bbf
heshrian <kamburovviktor@gmail.com>
2019-07-04 15:33:16 +0200

    lineplaysstart

---
+++ week-02/day-03/drawing/17 lineplays/lineplay.ts
@@ -0,0 +1,9 @@
+'use strict';
+export ={};
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+let daCanvasWidth:number=600;
+let daCanvasHeight:number=400;
+
+
