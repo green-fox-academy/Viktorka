c847ef129d9a44ef62c5944a4574e13b8edca591
heshrian <kamburovviktor@gmail.com>
2019-07-04 15:55:57 +0200

    functionforlineplays

--- week-02/day-03/drawing/17 lineplays/lineplay.ts
+++ week-02/day-03/drawing/17 lineplays/lineplay.ts
@@ -3,7 +3,24 @@ export ={};
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-let daCanvasWidth:number=600;
-let daCanvasHeight:number=400;
 
 
+
+ctx.moveTo(10,0)
+ctx.beginPath();
+for (let i:number =10; i < 610; i+=10){
+    
+    ctx.strokeStyle="purple";
+    ctx.lineTo(600,i);
+    ctx.stroke();
+    ctx.moveTo(20+i,0);
+
+}
+ctx.beginPath();
+for (let i2:number =10; i2 < 610; i2+=10){
+    
+    ctx.strokeStyle="green";
+    ctx.lineTo(i2,600);
+    ctx.stroke();
+    ctx.moveTo(0, 20+i2);
+}
