b09302f9d9270a2a80916eb9246e621d5e64bfa7
heshrian <kamburovviktor@gmail.com>
2019-07-16 15:29:38 +0200

    zoo is evil

--- week-04/day-02/zoo/reptile.ts
+++ week-04/day-02/zoo/reptile.ts
@@ -1,2 +1,19 @@
 'use strict';
 import Animal from './animals'
+
+export default class Reptile extends Animal {
+    coldblood: boolean;
+    tail: boolean;
+
+    constructor(name: string = "animal name", age: number = 0, gender: string = "random", blood?: boolean, taile?: boolean) {
+        super(name, age, gender);
+        this.coldblood = blood;
+        this.tail = taile;
+    }
+
+    breed(): string {
+        return `laying dem eggs`
+    }
+
+
+}
