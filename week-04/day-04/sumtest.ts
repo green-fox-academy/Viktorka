7c94dcb6d8f6479b39664dba9df352ae029d7b5c
heshrian <kamburovviktor@gmail.com>
2019-07-18 12:39:13 +0200

    test

--- week-04/day-04/sumtest.ts
+++ week-04/day-04/sumtest.ts
@@ -5,8 +5,8 @@ import test from 'tape'
 
 test(t => {
     let emptyArray: Sumable = new Sumable();
-    let oneElementArray: Sumable = new Sumable(1);
-    let twoElementArray: Sumable = new Sumable(3, 4);
+    let oneElementArray: Sumable = new Sumable();
+    let twoElementArray: Sumable = new Sumable();
 
 
 
