991aa2035fcdae025ffeb3da2be709fa1b4800d2
heshrian <kamburovviktor@gmail.com>
2019-08-08 18:05:29 +0200

    css added

--- orientation w6-10/week-06/day-04/gallery/index.js
+++ orientation w6-10/week-06/day-04/gallery/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 app.set('view engine', 'ejs');
-app.use(express.static('gallery'));
+app.use(express.static('images'));
+app.use(express.static('style'));
 
 app.get('/', (req, res) => {
     res.render('home', { imageList: imageList });
