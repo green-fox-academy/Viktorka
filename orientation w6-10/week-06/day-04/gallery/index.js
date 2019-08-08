e10860fdc09a0a3075b5140e9e4af7abb07bda8d
heshrian <kamburovviktor@gmail.com>
2019-08-08 12:57:42 +0200

    panda gallery, buttons no function

---
+++ orientation w6-10/week-06/day-04/gallery/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.set('view engine', 'ejs');
+app.use(express.static('images'));
+
+app.get('/', (req, res) => {
+    res.render('home', { imageList: imageList });
+});
+
+const imageList = [
+    { url: "1.jpg" },
+    { url: "2.jpg" },
+    { url: "3.jpg" },
+    { url: "4.jpg" },
+    { url: "5.jpg" },
+    { url: "6.jpg" },
+    { url: "7.jpg" },
+    { url: "8.jpg" },
+    { url: "9.jpg" }
+]
+
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`);
+});
