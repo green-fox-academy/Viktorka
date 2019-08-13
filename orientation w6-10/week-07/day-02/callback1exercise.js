43589e49f9820ee00140c83408e9f62517f78fd6
heshrian <kamburovviktor@gmail.com>
2019-08-13 13:56:10 +0200

    meh

--- orientation w6-10/week-07/day-02/callback1exercise.js
+++ orientation w6-10/week-07/day-02/callback1exercise.js
@@ -6,13 +6,8 @@ const mapWith = (array, callback) => {
     for (let i = 0; i < array.length; i++) {
         output.push(callback(array[i]));
     }
-
-    // The mapWith() function should iterate over the given array and call the callback function on every element.
-    // It stores the callback return values in the output.
-    // The mapWith() should return with the output array.
-
     return output;
-}
+};
 
 const addOne = (number) => {
     return number + 1;
@@ -29,36 +24,31 @@ console.log(mapWith([1, 2, 3], addOne));
 
 const words = ['map', 'reduce', 'filter'];
 
-const removeSecondLetter = (array) => {
-    let splittedChar = [];
-    for (let i = 0; i < array.length; i++) {
-        let array2 = [];
-        for (let j = 0; j < array[i].length; j++) {
-            if (j % 2 === 0) {
-                array2.push(array[i][j])
-            }
-        }
-        splittedChar.push(array2.join(''))
-        
-    }
-}
+// const removeSecondLetter = (array) => {
+//     let splittedChar = [];
+//     for (let i = 0; i < array.length; i++) {
+//         let array2 = [];
+//         for (let j = 0; j < array[i].length; j++) {
+//             if (j % 2 === 0) {
+//                 array2.push(array[i][j])
+//             }
+//         }
+//         splittedChar.push(array2.join(''))
 
+//     }
+// }
+
+const removeSecondLetter = (arrayElement) => {
+    let splittedWords = [];
+    for (let i = 0; i < arrayElement.length; i++) {
+        if (i % 2 == 0) {
+            splittedWords.push(arrayElement[i])
+            splittedWords.join('');
+        }
+    }
+    return splittedWords.join('')
+}
 console.log(mapWith(words, removeSecondLetter));
 
 
-// expected result: ['mp','rdc', 'fle']
 
-
-// let splittedChar = [];
-// for (let i = 0; i < words.length; i++) {
-//     let array2 = [];
-//     for (let j = 0; j < words[i].length; j++) {
-//         if (j % 2 === 0) {
-//             array2.push(words[i][j])
-//         }
-//     }
-//     splittedChar.push(array2.join(''))
-    
-// }
-
-// console.log(splittedChar)
