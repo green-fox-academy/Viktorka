b5d9491957614e3bea283747bdd1233a56b2bc52
heshrian <kamburovviktor@gmail.com>
2019-08-13 15:19:57 +0200

    prime or not here i come

--- orientation w6-10/week-07/day-02/callback1exercise.js
+++ orientation w6-10/week-07/day-02/callback1exercise.js
@@ -39,3 +39,11 @@ console.log(mapWith(words, removeSecondLetter));
 
 
 
+
+const isPrime = num => {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false; 
+    return num > 1;
+}
+
+console.log(isPrime(13))
