3a4b7a29feba560d171e45ccb5ff45b736f802de
heshrian <kamburovviktor@gmail.com>
2019-08-14 15:19:39 +0200

    dountil

--- orientation w6-10/week-07/day-03/1frontend/app.js
+++ orientation w6-10/week-07/day-03/1frontend/app.js
@@ -5,6 +5,21 @@ const path = require('path');
 app.use(express.json());
 app.use(express.static('assets'));
 
+
+const factorio = (input) => {
+    if (input == 0) {
+        return 1;
+    }
+    else {
+        let fact = 1;
+        for (let i = 1; i <= input; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+}
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -24,7 +39,6 @@ app.get('/greeter', (req, res) => {
     if (req.query.name === undefined && req.query.title === undefined) {
         res.json({
             "error": "Please provide a name and a title!"
-
         })
     } else if (req.query.name === undefined) {
         res.json({
@@ -41,8 +55,21 @@ app.get('/greeter', (req, res) => {
 
 app.get('/appenda/:word', (req, res) => {
     res.json({
-        "appended": "word" + "a",
+        "appended": req.params.word + "a",
     })
+})
+
+app.post('/dountil/:action', (req, res) => {
+    if (req.params.action === "sum") {
+        res.json({
+            "result": (req.body.until + 1) * req.body.until / 2
+        })
+    }
+    if (req.params.action === "factor"){
+        res.json({
+            "result" : factorio(req.body.until)
+        })
+    }
 })
 
 
