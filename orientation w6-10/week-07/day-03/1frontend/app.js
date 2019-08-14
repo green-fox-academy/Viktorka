7cd50e9f336afa1ed070174199c5293d3506d065
heshrian <kamburovviktor@gmail.com>
2019-08-14 13:34:26 +0200

    greeter

--- orientation w6-10/week-07/day-03/1frontend/app.js
+++ orientation w6-10/week-07/day-03/1frontend/app.js
@@ -10,21 +10,40 @@ app.get('/', (req, res) => {
 });
 
 app.get('/doubling', (req, res) => {
-    if (isNaN(parseInt(req.query.input))) {  
+    if (isNaN(parseInt(req.query.input))) {
         res.json({ "error": "Please provide an input!" })
     } else {
         res.json({
             "received": parseInt(req.query.input),
             "result": parseInt(req.query.input) * 2
         })
-    }  
+    }
 })
 
 app.get('/greeter', (req, res) => {
-    res.json({"welcome_message": "Oh, hi there " +req.query.name+", my dear "+req.query.title+"!"})
+    if (req.query.name === undefined && req.query.title === undefined) {
+        res.json({
+            "error": "Please provide a name and a title!"
+
+        })
+    } else if (req.query.name === undefined) {
+        res.json({
+            "error": "Please provide a name!"
+        })
+    } else if (req.query.title === undefined) {
+        res.json({
+            "error": "Please provide a title!"
+        })
+    } else {
+        res.json({ "welcome_message": "Oh, hi there " + req.query.name + ", my dear " + req.query.title + "!" })
+    }
 })
 
-
+app.get('/appenda/:word', (req, res) => {
+    res.json({
+        "appended": "word" + "a",
+    })
+})
 
 
 
