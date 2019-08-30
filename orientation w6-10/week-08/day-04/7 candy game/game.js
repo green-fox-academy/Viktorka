d207dadd92f5ea58548874c5b82ee225a6eabb73
heshrian <kamburovviktor@gmail.com>
2019-08-30 09:24:11 +0200

    candy shop is maxed out?

--- orientation w6-10/week-08/day-04/7 candy game/game.js
+++ orientation w6-10/week-08/day-04/7 candy game/game.js
@@ -6,49 +6,60 @@ let candyRain = document.querySelector('.candy-machine');
 let actualCandies = document.querySelector('.candies');
 let pops = document.querySelector('.lollypops');
 let speed = document.querySelector('.speed');
-let timer = 5000
-
+let timer = 15 * 1000;
+let countDown = timer / 1000;
 let speedCounter = 0;
 let candyCounter = 0;
 let popCounter = 0;
+buyLollipops.disabled = true;
+let popPrice = 30;
+buyLollipops.textContent = `Buy Lollipops (${popPrice}🍬)`;
+
+
 function actualCandyNum() {
     actualCandies.textContent = candyCounter;
 }
 function addPop() {
-    pops.textContent += "🍭";
+    if (pops.textContent === "0") {
+        pops.textContent = "🍭";
+    } else {
+        pops.textContent += "🍭";
+    }
     popCounter++;
 }
 
 function addOneCandy() {
     candyCounter += 1;
     actualCandyNum();
+    if (candyCounter >= popPrice) {
+        buyLollipops.disabled = false;
+    }
 }
 
-
-
 function buyPops() {
-    if (candyCounter >= 5) {
-        candyCounter -= 5;
+    if (candyCounter >= popPrice) {
+        buyLollipops.disabled = false;
+        candyCounter -= popPrice;
         actualCandyNum();
         addPop();
+        buyLollipops.disabled = true;
         if (popCounter % 2 === 0 && popCounter !== 0) {
             speedCounter++;
             setInterval(addOneCandy, 1000);
             speed.textContent = speedCounter;
         }
+    }
+}
 
+function theCandyShop() {
+    if (candyCounter >= 1000) {
+        document.body.setAttribute('background', 'https://cdn.unifiedcommerce.com/content/product/large/079346117475.jpeg');
     }
 }
 
 function candyRainRestart() {
-    
     candyRain.disabled = false;
-    // candyRain.textContent = 'Make candy rain';
-}
-
-function coolDownLol() {
-    // candyRain.textContent = `will be ready shortly...`;
-    setTimeout(candyRainRestart,timer-900)
+    candyRain.textContent = 'Make candy rain';
 }
 
 function tenTimes() {
@@ -59,12 +70,24 @@ function tenTimes() {
             setInterval(addOneCandy, 1000);
             speed.textContent = speedCounter;
             candyRain.disabled = true;
-            setTimeout(candyRainRestart, timer);
-            // setInterval(coolDownLol, 100);
+            // setTimeout(candyRainRestart, timer);
         }
+        let countDowner = setInterval(() => {
+            countDown--;
+            candyRain.textContent = `${countDown}`;
+            console.log(countDown);
+            setTimeout(candyRainRestart, timer);
+            candyRain.disabled = true;
+            if (countDown <= 0) {
+                // candyRain.textContent = 'Make candy rain';
+                clearInterval(countDowner);
+            }
+        }, 1000);
+        countDown = timer / 1000;
     }
 }
 
 candies.addEventListener('click', addOneCandy);
 buyLollipops.addEventListener('click', buyPops);
 candyRain.addEventListener('click', tenTimes);
+setInterval(theCandyShop,1000)
