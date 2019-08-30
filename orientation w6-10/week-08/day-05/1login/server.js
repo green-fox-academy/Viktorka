0adc96f099be2ad8e76793036002da4a5a06f076
heshrian <kamburovviktor@gmail.com>
2019-08-30 14:50:22 +0200

    got the login details, but can't do jack with them

--- orientation w6-10/week-08/day-05/1login/server.js
+++ orientation w6-10/week-08/day-05/1login/server.js
@@ -8,19 +8,20 @@ app.use(express.static('views'))
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile('/views/login.html', {root :__dirname})
+    res.sendFile('/views/login.html', { root: __dirname });
 })
 
+app.post('/send', (req, res) => {
+    console.log(`Username ${req.body.username}`);
+    console.log(`Password ${req.body.password}`);
+    if (req.body.username === 'asd' && req.body.password === 'asd') {
+        console.log('correct');
+    } else {
+        console.log('incorrect')
+    }
+    // alert(`Your password for ${req.body.username} is correct`);
 
-
-
-
-
-
-
-
-
-
+})
 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
---
+++ orientation w6-10/week-08/day-05/1login/views/game.js
@@ -0,0 +1,100 @@
+'use strict';
+
+let candies = document.querySelector('.create-candies');
+let buyLollipops = document.querySelector('.buy-lollypops');
+let candyRain = document.querySelector('.candy-machine');
+let actualCandies = document.querySelector('.candies');
+let pops = document.querySelector('.lollypops');
+let speed = document.querySelector('.speed');
+let timer = 15 * 1000;
+let countDown = timer / 1000;
+let speedCounter = 0;
+let candyCounter = 0;
+let popCounter = 0;
+buyLollipops.disabled = true;
+let popPrice = 30;
+let lolliesForCandiPerSec=10;
+candyRain.textContent= `Make candy rain (${countDown} sec)`
+buyLollipops.textContent = `Buy Lollipops (${popPrice}🍬)`;
+let div = document.querySelector('.container');
+let extraInfo = document.createElement('p');
+extraInfo.textContent=`Buy ${lolliesForCandiPerSec} lollypops to increase your Candy/Second!`
+div.appendChild(extraInfo);
+
+
+function actualCandyNum() {
+    actualCandies.textContent = candyCounter;
+}
+function addPop() {
+    if (pops.textContent === "0") {
+        pops.textContent = "🍭";
+    } else {
+        pops.textContent += "🍭";
+    }
+    popCounter++;
+}
+
+function addOneCandy() {
+    candyCounter += 1;
+    actualCandyNum();
+    if (candyCounter >= popPrice) {
+        buyLollipops.disabled = false;
+    }
+}
+
+function buyPops() {
+    if (candyCounter >= popPrice) {
+        buyLollipops.disabled = false;
+        candyCounter -= popPrice;
+        actualCandyNum();
+        addPop();
+        buyLollipops.disabled = true;
+        if (popCounter % lolliesForCandiPerSec === 0 && popCounter !== 0) {
+            speedCounter++;
+            setInterval(addOneCandy, 1000);
+            speed.textContent = speedCounter;
+        }
+    }
+}
+
+function theCandyShop() {
+    if (candyCounter >= 1000) {
+        document.body.setAttribute('background', 'https://cdn.unifiedcommerce.com/content/product/large/079346117475.jpeg');
+        div.setAttribute('bgcolor', 'white');
+    }
+}
+
+function candyRainRestart() {
+    candyRain.disabled = false;
+    candyRain.textContent = 'Make candy rain';
+}
+
+function tenTimes() {
+    if (speedCounter !== 0) {
+        speed.textContent = speedCounter * 10;
+        speedCounter = speedCounter * 10;
+        for (let i = 0; i <= speedCounter; i++) {
+            setInterval(addOneCandy, 1000);
+            speed.textContent = speedCounter;
+            candyRain.disabled = true;
+            // setTimeout(candyRainRestart, timer);
+        }
+        let countDowner = setInterval(() => {
+            countDown--;
+            candyRain.textContent = `${countDown}`;
+            console.log(countDown);
+            setTimeout(candyRainRestart, timer);
+            candyRain.disabled = true;
+            if (countDown <= 0) {
+                // candyRain.textContent = 'Make candy rain';
+                clearInterval(countDowner);
+            }
+        }, 1000);
+        countDown = timer / 1000;
+    }
+}
+
+candies.addEventListener('click', addOneCandy);
+buyLollipops.addEventListener('click', buyPops);
+candyRain.addEventListener('click', tenTimes);
+setInterval(theCandyShop,100)
