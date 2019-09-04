51e620c887f3725736f2e1c63a47add946d2de44
heshrian <kamburovviktor@gmail.com>
2019-09-04 14:35:46 +0200

    arrow finished

--- orientation w6-10/week-09/day-03/iamgroot/routes.js
+++ orientation w6-10/week-09/day-03/iamgroot/routes.js
@@ -3,15 +3,38 @@ const app = express();
 
 app.get('/groot', (req, res) => {
     // TODO: implement this method
-    console.log(req.query.message.length)
-    if (req.query.message.length===0) {
-        res.json({ "error": "I am Groot!" })
+    console.log(req.query.message)
+    if (req.query.message) {
+        res.json({
+            'received': req.query.message,
+            'transalated': 'I am Groot!'
+        })
     }
     else {
-        res.json({
-            "received": req.query.message,
-            "transalated": "I am Groot!"
-        })
-    };
+        res.status(400)
+        res.json({ 'error': 'I am Groot!' })
+
+    }
+
 })
+app.get('/yondu', (req, res) => {
+    console.log(Number(req.query.time))
+    if (req.query.distance && req.query.time && Number(req.query.time)!==0 ) {
+        res.json({
+            'distance': req.query.distance,
+            'time': req.query.time,
+            'speed': req.query.distance / req.query.time
+        })
+    } else if (Number(req.query.time)===0 ) {
+        res.status(400)
+        res.json({
+            'distance': req.query.distance,
+            'time': req.query.time,
+            'speed': 'Incalculable'
+        })
+    } else {
+        res.status(400)
+    }
+})
+
 module.exports = app;
