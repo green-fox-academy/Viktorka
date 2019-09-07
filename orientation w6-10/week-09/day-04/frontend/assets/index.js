b15b6b3f9fa82942d9c34cf97a98f2bc5f07cae9
heshrian <kamburovviktor@gmail.com>
2019-09-07 11:06:30 +0200

    delete any post

--- orientation w6-10/week-09/day-04/frontend/assets/index.js
+++ orientation w6-10/week-09/day-04/frontend/assets/index.js
@@ -12,20 +12,97 @@ function redirect() {
     window.location = 'http://localhost:2999/post'
 }
 
-const id = document.querySelector('.hide');
-const form = document.querySelector('.delete');
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            console.log(id.textContent)
-            location.href = 'http://localhost:2999'
-        }
-    }
+//working to delete top post
+const form = document.querySelectorAll('.delete');
+form.forEach(element => {
 
-xhr.open('POST', '/');
-xhr.setRequestHeader('Content-Type', 'application/json')
-xhr.send(JSON.stringify({id: id.textContent}))
-form.reset()
-});
+    element.addEventListener('submit', e => {
+        e.preventDefault();
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                console.log(element.textContent)
+                location.href = 'http://localhost:2999'
+            }
+        }
+
+        xhr.open('POST', '/');
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.send(JSON.stringify({ id: element.textContent }))
+        form.reset()
+    });
+})
+
+
+// const id = document.querySelector('.hide');
+
+
+// const id = document.querySelectorAll('.hide');
+
+// id.forEach(e =>{
+    //     e.addEventListener('submit', event =>{
+//         event.preventDefault();
+//             const xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = () => {
+//         if (xhr.status >= 200 && xhr.status < 300) {
+//             console.log(id.textContent)
+//             // location.href = 'http://localhost:2999'
+//         }
+//     }
+//     })
+// })
+
+// const id = document.querySelector('.hide');
+
+// for (let i = 0; i < smallPix.length; i++) {
+//     smallPix[i].onclick = () => { //(event)
+//         mainImg.src = smallPix[i].src; //event.target.src
+//         document.querySelector(".currentImage").classList.remove("currentImage");
+//         smallPix[i].classList.add('currentImage');
+//         changeText();
+//     }
+// }
+// const form = document.querySelectorAll('.delete');
+// // const deleteMe = document.querySelector('.delete').classList.add('toDelete');
+
+// const toDelete = document.querySelector('.toDelete')
+// if (toDelete !== null) {
+//     for (let i = 0; i < form.length; i++) {
+//         form[i].onclick = () => {
+//             // document.querySelector('.toDelete').classList.remove('toDelete')
+//             form[i].classList.add('toDelete')
+//         }
+//     }
+//     toDelete.addEventListener('submit', e => {
+//         e.preventDefault();
+//         const xhr = new XMLHttpRequest();
+//         xhr.onreadystatechange = () => {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 console.log(toDelete.textContent)
+//                 location.href = 'http://localhost:2999'
+//             }
+//         }
+
+//         xhr.open('POST', '/');
+//         xhr.setRequestHeader('Content-Type', 'application/json')
+//         xhr.send(JSON.stringify({ id: toDelete.textContent }))
+//         form.reset()
+//     });
+// }
+
+// form.forEach(element => {
+//     element.addEventListener('submit', e => {
+//         e.preventDefault();
+//         const xhr = new XMLHttpRequest();
+//         xhr.onreadystatechange = () => {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 console.log(element.textContent)
+//                 // location.href = 'http://localhost:2999'
+//             }
+//         }
+//         xhr.open('POST', '/');
+//         xhr.setRequestHeader('Content-Type', 'application/json')
+//         xhr.send(JSON.stringify({ id: element.textContent }))
+//         form.reset()
+//     });
+// })
