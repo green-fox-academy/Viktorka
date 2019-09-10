64e2310fa7d2bc06d38b63a6238cfb33f68bdab8
heshrian <kamburovviktor@gmail.com>
2019-09-10 12:27:10 +0200

    current song updates

--- orientation w6-10/week-10/music player/assets/audio.js
+++ orientation w6-10/week-10/music player/assets/audio.js
@@ -7,6 +7,15 @@ const prev = document.querySelector('.prev');
 const playlist = document.querySelectorAll('.hide')
 let actualSong = document.querySelector('.hide')
 actualSong.classList.add('actual')
+const current = document.querySelector('.current')
+let activeTitle = document.querySelector('.activeTitle')
+activeTitle.textContent=actualSong.parentElement.innerText   
+console.log(actualSong.parentElement.innerText)
+
+
+
+// console.log(title.textContent.split(' ')[0])
+// current.textContent=title.textContent.split(' ')[0]
 
 // play.onclick =() => {
 // play.play()
@@ -18,18 +27,24 @@ audio.addEventListener('ended', () => console.log('ended happened'));
 audio.addEventListener('progress', () => console.log('progress happened'));
 
 let nextSong = () => {
-    let nextSong = document.querySelector('.actual').parentElement.nextElementSibling.childNodes[3];
-    if (document.querySelector('.actual').parentElement.nextElementSibling === null) {
-        nextSong = document.querySelector('.hide')
+    let nextSong = document.querySelector('.actual').parentElement.nextElementSibling.childNodes[3]
+    if (audio.src === 'http://localhost:3123/2.mp3') {
+        next.disabled=true
     }
+    // let theNextSong = nextSong.childNodes[3]
     let removeMe = document.querySelector('.actual')
     audio.src = nextSong.textContent;
     removeMe.classList.remove('actual');
     nextSong.classList.add('actual');
-    // console.log(removeMe)
+    console.log(nextSong.parentElement.innerText)
+    
+    activeTitle.textContent=nextSong.parentElement.innerText
+    // activeTitle.textContent=
+    // console.log(audio.src)
 }
 let prevSong = () => {
     console.log('prev song')
+    // same as next
 }
 next.addEventListener('click', nextSong);
 prev.addEventListener('click', prevSong);
