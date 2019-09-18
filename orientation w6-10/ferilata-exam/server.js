cc7ff4db7cfbee5c2473505e1e40fc13f74f84eb
heshrian <kamburovviktor@gmail.com>
2019-09-18 16:13:15 +0200

    done?

--- orientation w6-10/ferilata-exam/server.js
+++ orientation w6-10/ferilata-exam/server.js
@@ -17,7 +17,7 @@ const connection = mysql.createConnection({
 
 const app = express();
 app.use(express.json());
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 app.use(express.static('views'));
 
 app.get('/', (req, res) => {
@@ -35,7 +35,35 @@ app.get('/api/items', (req, res) => {
   })
 })
 
+app.post('/api/items/:id', (req, res) => {
+  connection.query(`SELECT * from items
+  WHERE id=?;`, [req.params.id], (err, resp) => {
+    if (err) {
+      console.log(err.message)
+      res.sendStatus(500)
+    } else {
+      if (new Date() > resp[0].expiryDate) {
+        res.send({ "message": "expired" });
+      } else if (req.body.amount < resp[0].highestBid) {
+        res.send({ "message": "Your bid is below the highest bid!" })
+      } else if (req.body.amount > resp[0].highestBid){
+        connection.query(`UPDATE items
+        SET highestBid=?,
+        highestBidderName=?
+        WHERE id=?;`,[req.body.amount,req.body.name,req.params.id], (err, rows) => {
+          if (err) {
+            console.log(err.message)
+            res.sendStatus(500);
+          } else {
+            res.send({message:'Success! oh yeah'})
+          }
+        })
+      }
+    }
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}`);
 });
+
--- orientation w6-10/ferilata-exam/views/main.js
+++ orientation w6-10/ferilata-exam/views/main.js
@@ -1,16 +1,19 @@
 'use strict';
-let select = document.querySelector('select');
-let ul = document.querySelector('ul');
+const select = document.querySelector('select');
+const ul = document.querySelector('ul');
+const form = document.querySelector('form');
+const bider = document.getElementById('name');
+const bid = document.getElementById('amount');
 
 
 window.onload = function loadData() {
   fetch('/api/items')
     .then(response => response.json())
-    .then(response2 => {
-      let myJson = response2;
-      myJson.forEach(e => {
+    .then(response => {
+      response.forEach(e => {
         let option = document.createElement('option');
         option.textContent = e.title;
+        option.value = e.id;
         select.appendChild(option);
         let liElem = document.createElement('li');
         liElem.textContent = `${e.title}(highest bid: ${e.highestBid}, ${e.highestBidderName})`;
@@ -18,3 +21,43 @@ window.onload = function loadData() {
       })
     })
 }
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   const xhr = new XMLHttpRequest();
+//   xhr.responseType = 'json';
+//   xhr.onreadystatechange = () => {
+//     if (xhr.readyState === XMLHttpRequest.DONE) {
+//       if (xhr.status >= 200 && xhr.status < 300) {
+//         form.reset();
+//         let pElem = document.createElement('p');
+//         pElem.textContent = response.message;
+//         document.querySelector('body').appendChild(pElem);
+
+//       } else {
+//         alert('some error handling');
+//       }
+//     }
+//   }
+//   xhr.open('POST', `/api/items/${select.value}`);
+//   xhr.setRequestHeader('Content-Type', 'application/json');
+//   xhr.send(JSON.stringify({ id: select.value, name: bider.value, amount: bid.value }));
+// })
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  fetch(`/api/items/${select.value}`, {
+    method: 'POST',
+    body: JSON.stringify({ id: select.value, name: bider.value, amount: bid.value }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  }).then(response => response.json())
+    .then(response => {
+      let pElem = document.createElement('p');
+      pElem.textContent = response.message;
+      document.querySelector('body').appendChild(pElem);
+      form.reset();
+      // window.location.reload();
+    });
+})
