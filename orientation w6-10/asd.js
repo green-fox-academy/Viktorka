cae1139fcf9682795a64c69d212dd5f8bca84855
heshrian <kamburovviktor@gmail.com>
2019-09-26 17:43:27 +0200

    codewars sucks

--- orientation w6-10/asd.js
+++ orientation w6-10/asd.js
@@ -1,3 +1,4 @@
+'use strict'
 // is isogram => one letter only appear once
 // function isIsogram(str) {
 //   //...
@@ -82,19 +83,159 @@
 // console.log(createPhoneNumber(asd))
 
 
-function divisors(integer) {
-  let answer = [];
-  // let prime = ''
-  for (let i = 2; i <= integer; i++) {
-    if (integer % i === 0 && i !== integer) {
-      answer.push(i);
-    } else if (i === integer && integer % i !== 0) {
+// function divisors(integer) {
+//   let answer = [];
+//   // let prime = ''
+//   for (let i = 2; i <= integer; i++) {
+//     if (integer % i === 0 && i !== integer) {
+//       answer.push(i);
+//     } else if (i === integer && integer % i !== 0) {
 
-    }
-  } if (answer.length === 0) {
-    return `${integer} is prime`
-  } else {
-    return answer
-  }
+//     }
+//   } if (answer.length === 0) {
+//     return `${integer} is prime`
+//   } else {
+//     return answer
+//   }
+// }
+// console.log(divisors(14))
+
+
+// console.log(Number(16).toString(2))
+
+// var countBits = function (n) {
+//   // Program Me
+//   let binNum = Number(n).toString(2).split('');
+//   let counter = 0;
+//   binNum.forEach(e => {
+//     if (e==='1'){
+//       counter++
+//     }
+//   })
+
+
+//   return counter
+// };
+
+// move zeros from array to the end
+// var moveZeros = function (arr) {
+//   let arr1 = [];
+//   let counter = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== 0) {
+//       arr1.push(arr[i]);
+//     } else {
+//       counter++
+//     }
+//   }
+//   console.log(counter)
+//   for (let j = 0; j < counter; j++) {
+//     arr1.push(0);
+//   }
+//   return arr1
+// }
+
+
+// var moveZeros = function (arr) {
+//   let arr1 = [];
+//   let counter = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== 0) {
+//       arr1.push(arr[i]);
+//     } else {
+//       counter++
+//     }
+//   }
+//   console.log(counter)
+//   for (let j = 0; j < counter; j++) {
+//     arr1.push(0);
+//   }
+//   return arr1
+// }
+
+
+// how many times can see the ball from h tall drop with bounce (between 0-1(like 0.66)) from the window that is less than h tall.
+// function bouncingBall(h, bounce, window) {
+//   if (h > 0 && bounce > 0 && bounce < 1 && window < h) {
+//     if (h > window) {
+//       h = h * bounce
+//     }
+//     if (h < window) {
+//       return 1;
+//     } else {
+//       return 2 + bouncingBall(h, bounce, window);
+//     }
+//   } else {
+//     return -1
+//   }
+// }
+
+// if str is even pair them str[0]str[1], str[2]str[3] if odd str[0]str[1], str[2]_
+// function solution(str) {
+//   let arr = str.split('');
+//   let arr1 = []
+//   for (let i = 0; i < arr.length; i = i + 2) {
+//     if (arr[i + 1] === undefined) {
+//       arr1.push(arr[i] + '_')
+//     } else
+//       arr1.push(arr[i] + arr[i + 1])
+//   } return arr1
+// }
+// console.log(solution('abc'))
+
+// 
+// function deleteNth(arr, n) {
+//   // ...
+//   let countObj = {}
+//   let arr1 = []
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] in countObj) {
+//       countObj[arr[i]]++;
+//     } else {
+//       countObj[arr[i]] = 1;
+//     }
+//     if (countObj[arr[i]] <= n) {
+//       arr1.push(arr[i]);
+//     } else {
+
+//     }
+//   }
+//   return arr1
+// }
+// console.log(deleteNth([1, 2, 3, 1, 2, 2], 1))
+
+
+function persistence(num) {
+  //code me
+  let counter = 0;
+  for (let i = 9; i > 0; i--) {
+    if (num % i === 0) {
+      counter++;
+      break
+    } 
+    persistence(num / i)
+  } return counter
+
 }
-console.log(divisors(14))
+
+console.log(persistence(155))
+
+//TODO
+// function isPrime(num) {
+//   if (num === 1) {
+//     return false;
+//   }
+//   let arr = [];
+//   for (let i = 2; i <= num; i++) {
+//     if (num % i === 0) {
+//       arr.push[i];
+//     }
+//     console.log(arr)
+//     if (arr.length > 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+// console.log(isPrime(4))
