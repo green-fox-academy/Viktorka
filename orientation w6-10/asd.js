434ea0b42c3aea557bd29ad8e77a46edb8329321
heshrian <kamburovviktor@gmail.com>
2019-10-01 17:46:25 +0200

    hackerrank

--- orientation w6-10/asd.js
+++ orientation w6-10/asd.js
@@ -204,23 +204,23 @@
 // }
 // console.log(deleteNth([1, 2, 3, 1, 2, 2], 1))
 
+//not ok
+// function persistence(num) {
+//   //code me
+//   let counter = 0;
+//   for (let i = 9; i > 0; i--) {
+//     if (num % i === 0) {
+//       counter++;
+//       break
+//     } 
+//     persistence(num / i)
+//   } return counter
 
-function persistence(num) {
-  //code me
-  let counter = 0;
-  for (let i = 9; i > 0; i--) {
-    if (num % i === 0) {
-      counter++;
-      break
-    } 
-    persistence(num / i)
-  } return counter
+// }
 
-}
+// console.log(persistence(155))
 
-console.log(persistence(155))
-
-//TODO
+//TODO not ok
 // function isPrime(num) {
 //   if (num === 1) {
 //     return false;
@@ -239,3 +239,156 @@ console.log(persistence(155))
 //   }
 // }
 // console.log(isPrime(4))
+
+
+
+// function staircase(x) {
+//   let a = ' ';
+//   for (let i = 1; i <= x; i++) {
+//     console.log(a.repeat(x - i) + '#'.repeat(i));
+//   }
+// }
+
+
+// staircase(4)
+let array1 = [1, 3, 2, 3]
+
+// function miniMaxSum(arr) {
+//   let sum1 = 0;
+//   let sum2 = 0;
+//   let newArr = arr.sort();
+//   for (let i = 1; i < newArr.length; i++) {
+//     sum2 += newArr[i]
+//   }
+//   for (let i = 0; i < newArr.length - 1; i++) {
+//     sum1 += newArr[i]
+//   }
+//   console.log(sum1, sum2)
+// }
+
+// miniMaxSum(array1)
+
+let time = '12:45:45AM'
+let time2 = '11:59:12PM'
+
+// function timeConversion(s) {
+//   let s2 = s.split('');
+//   if (s2[8] === 'P' && (s2[1] !== '1' || s2[0] !== '2')) {
+//     return `${Number.parseInt(s2[1]) + 12}${s2[2]}${s2[3]}${s2[4]}${s2[5]}${s2[6]}${s2[7]}`
+//   } else {
+//     return `${0}${Number.parseInt(s2[1])-2}${s2[2]}${s2[3]}${s2[4]}${s2[5]}${s2[6]}${s2[7]}`
+//   }
+// }
+//   let answer = '';
+//   let s2 = s.split(':');
+//   let s4 = s.slice(0, 8).split(':');
+//   console.log(s2)
+//   let s3 = s2[2].split('')
+//   if (s3[2] === 'P' && Number.parseInt(s2[0]) < 12) {
+//     answer += `${Number.parseInt(s4[0]) + 12}:${s4[1]}:${s4[2]}`;
+//   } else if (s3[2] === 'A' && Number.parseInt(s2[0]) < 12) {
+//     answer += `${s4[0]}:${s4[1]}:${s4[2]}`
+//   } else if (s3[2]==='P' && Number.parseInt(s2[0])===12){
+//     answer += `12:${s4[1]}:${s4[2]}`
+//   } else if (s3[2]==='A' && Number.parseInt(s2[0])===12){
+//     answer += `00:${s4[1]}:${s4[2]}`
+//   }
+
+
+//   console.log(answer)
+// }
+// timeConversion(time)
+// // console.log(timeConversion(time2))
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+// let arrr = [4, 73, 67, 38, 33]
+
+// function gradingStudents(grades) {
+//   // Write your code here
+//   let annsw = []
+//   for (let i = 1; i < grades.length; i++) {
+//     if (grades[i] > 37 && (grades[i] + 1) % 5 === 0) {
+//       annsw.push(grades[i]+1);
+//     } else if (grades[i] > 37 && (grades[i] + 2) % 5 === 0){
+//       annsw.push(grades[i]+2);
+//     } else if (grades[i]<= 37){
+//       annsw.push(grades[i])
+//     } else {
+//       annsw.push(grades[i])
+//     }
+//   } return annsw
+// }
+
+// console.log(gradingStudents(arrr))
+
+// let sc = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
+
+// function breakingRecords(scores) {
+//   let highest = scores[0];
+//   let lowest = scores[0];
+//   let highestC = 0;
+//   let lowestC = 0;
+//   for (let i = 1; i < scores.length; i++) {
+//     if (scores[i] > highest) {
+//       highest = scores[i]
+//       highestC++
+//     } else if (scores[i] < lowest) {
+//       lowest = scores[i]
+//       lowestC++
+//     }
+//   }
+//   return [highestC,lowestC]
+// }
+
+// console.log(breakingRecords(sc))
+
+// let billl = [3, 10, 2, 9]
+
+
+// function bonAppetit(bill, k, b) {
+//   const annasBill = (bill.reduce((a, b) => a + b, 0) - bill[k]) / 2;
+//   annasBill === b ? console.log('Bon Appetit') : console.log(b - annasBill);
+// }
+
+// bonAppetit(billl, 1,12)
+
+// billl[2]===2? console.log('yes'): console.log('no');
+
+let sox = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+
+function sockMerchant(n, ar) {
+  ar.sort();
+  let pairs = []
+  for (let i = 0; i < n; i++) {
+      if (ar[i] === ar[i + 1]) {
+        pairs.push(ar[i]);
+        pairs.push(ar[i + 1])
+        ar.splice(i, 1);
+        n--;
+      }
+  }
+  console.log(ar,pairs,pairs.length/2)
+}
+
+sockMerchant(9, sox)
+
+// let s = ["UDDDUDUU"]
+
+// function countingValleys(n, s) {
+//   let counterU = 0;
+//   let counterD = 0;
+//   let as = s.split('')
+//   console.log(as)
+//   for (let i = 0; i < n; i++) {
+
+//   }
+
+// }
+
+// countingValleys(8, s)
